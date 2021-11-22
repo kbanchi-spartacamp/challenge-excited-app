83014bf3afa163189774d0f0ef874cc9f0e0dec0
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-22 23:14:36 +0900

    add heroku config

--- database/seeders/AvatorImageSeeder.php
+++ database/seeders/AvatorImageSeeder.php
@@ -30,6 +30,21 @@ class AvatorImageSeeder extends Seeder
                 'level' => -1,
                 'image_path' => 'kung-fu_man.png'
             ],
+            [
+                'avator_category_id' => 1,
+                'level' => 10,
+                'image_path' => 'yuusya_game.png'
+            ],
+            [
+                'avator_category_id' => 2,
+                'level' => 10,
+                'image_path' => 'mahoutsukai_man.png'
+            ],
+            [
+                'avator_category_id' => 3,
+                'level' => 10,
+                'image_path' => 'kung-fu_man.png'
+            ],
         ];
         DB::table('avator_images')->insert($param);
     }
